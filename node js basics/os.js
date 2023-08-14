eed384ce781cf2698ccee1d9dfcc49857f12e885
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-15 00:18:36 +0500

    os  module completed

--- node js basics/os.js
+++ node js basics/os.js
@@ -17,3 +17,26 @@ console.log(
 console.log(os.arch());
 console.log(os.cpus());
 console.log(os.endianness());
+console.log(os.freemem());
+console.log(os.getPriority());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.loadavg());
+console.log(os.networkInterfaces());
+console.log(os.platform());
+console.log(os.release());
+console.log("setting priority for" + " the current process to 13");
+try {
+  // Setting priority of current process
+  os.setPriority(13);
+} catch (err) {
+  // Printing error message if any
+  console.log(": error occurred" + err);
+}
+
+console.log(os.tmpdir());
+console.log(os.totalmem());
+console.log(os.type());
+console.log(os.uptime());
+console.log(os.userInfo());
+console.log(os.version());
