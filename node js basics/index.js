ba3bac47c0f3cb177e50d972c7615b1f618eae3b
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-14 14:11:33 +0500

    node basics

---
+++ node js basics/index.js
@@ -0,0 +1,6 @@
+console.log("Lets run it");
+console.log(200 / 3);
+let a = 12;
+let b = 23;
+c = a + b;
+console.log(c);
---
+++ node js basics/server.js
@@ -0,0 +1,18 @@
+console.log("lets make a server");
+const http = require("http");
+console.log(http);
+const server = http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.end("<h1>Home Page is rendering</h1>");
+    }
+    if (req.url === "/about") {
+      res.end("<h1>About Page is rendering</h1>");
+    }
+    if (req.url === "/contact") {
+      res.end("<h1>Contact Page is rendering</h1>");
+    } else {
+      res.end("<h1>Something is rendering</h1>");
+    }
+  })
+  .listen(4000, () => console.log("Server is working"));
