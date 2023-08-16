be25a5466ef8d7563dd97edad38c9709fbfe2daa
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-17 00:43:35 +0500

    mongodb completed

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbName: "backend",
+  })
+  .then(() => console.log("connected"))
+  .catch((e) => console.log(e));
+
+const messageSchema = mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const message = mongoose.model("names", messageSchema);
 let users = [];
 app.listen(4000, () => {
   console.log("working");
@@ -13,6 +28,15 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { name: "Mati" });
+});
+
+app.post("/add", async (req, res) => {
+  const data = await message.create({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  data.save();
+  res.send("data saved");
 });
 app.post("/signup", (req, res) => {
   console.log(req.body);
