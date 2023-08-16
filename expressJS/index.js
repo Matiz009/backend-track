adb6490a91af8f6f669c802fc0f32fb19586ab98
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-16 20:09:03 +0500

    ejs  module completed

--- expressJS/index.js
+++ expressJS/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+let users = [];
 app.listen(4000, () => {
   console.log("working");
 });
 app.use(express.static(path.join(path.resolve(), "public")));
+//accessing the middleware
+app.use(express.urlencoded({ extended: true }));
+
 //setting up view engine
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index", { name: "Mati" });
+});
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  users.push(req.body);
+  res.end("Done");
+});
+app.get("/users", (req, res) => {
+  res.json(users);
 });
