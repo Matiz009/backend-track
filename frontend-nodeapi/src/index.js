7d4e824baa5a8e935b456f7ff9d7131f2c403f34
MATI UL REHMAN <iammatiz00@gmail.com>
2023-08-20 20:14:49 +0500

    login signup completed

--- frontend-nodeapi/src/index.js
+++ frontend-nodeapi/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+export const server = "http://localhost:4000";
 root.render(
   <React.StrictMode>
     <App />
--- frontend-nodeapi/src/pages/Login.jsx
+++ frontend-nodeapi/src/pages/Login.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { server } from "../index";
 
 const Login = () => {
-  return <div> Login </div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        `${server}/users/login`,
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="login">
+      <section>
+        <form onSubmit={submitHandler}>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+          <h4>Or</h4>
+          <Link to="/register">Sign Up</Link>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
